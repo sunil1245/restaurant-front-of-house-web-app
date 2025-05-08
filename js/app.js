532c46f35ae8b1fc561b2f244b1950229df25cc1
sunil1245 <119752484+sunil1245@users.noreply.github.com>
2025-05-07 21:52:04 -0500

    Adding three timers

--- js/app.js
+++ js/app.js
@@ -1,29 +1,80 @@
 //const setTaskInterval = setInterval(getIceForDriveThru, 5000);
-let countDown = 20;
-console.log(countDown)
-renderCounter();
+let iceForDriveThruCountDown = 20;
+let diningRoomCheckCountDown = 30;
+let emptyTrashCansCountDown = 45;
 
-setInterval(updateTimer, 1000);
+renderCounterIceForDriveThru();
+renderCounterDiningRoomCheck();
+renderCounterEmptyTrashCans();
+
+
+
+setInterval(updateIceForDriveThruTimer, 1000);
+setInterval(updateDiningRoomCheckTimer, 1000);
+setInterval(updateEmptyTrashCansTimer, 1000);
 
 function getIceForDriveThru() {
     alert("Take ice to Drive-Thru");
 }
 
+function resetIceDriveThruCounter() {
+    iceForDriveThruCountDown = 20;
+    renderCounterIceForDriveThru();
+}
+
+function resetDiningRoomCounter() {
+    diningRoomCheckCountDown = 30;
+    renderCounterDiningRoomCheck();
+}
+
+function resetEmptyTrashCansCounter() {
+    emptyTrashCansCountDown = 45;
+    renderCounterEmptyTrashCans();
+}
+
 function displaySuccessMessage() {
-    countDown = 20;
-    renderCounter();
+    resetIceDriveThruCounter();
+    resetDiningRoomCounter();
+    resetEmptyTrashCansCounter();
 }
 
-function updateTimer() {
-    if (countDown > 0) {
-        countDown--;
-        console.log(countDown);
+function updateIceForDriveThruTimer() {
+    if (iceForDriveThruCountDown > 0) {
+        iceForDriveThruCountDown--;
+        //console.log(countDown);
     }
-
-    renderCounter();
+    renderCounterIceForDriveThru();
 }
 
-function renderCounter() {
-    const counterElement = document.getElementById("counter");
-    counterElement.innerHTML = countDown;
+function updateDiningRoomCheckTimer() {
+    if (diningRoomCheckCountDown > 0) {
+        diningRoomCheckCountDown--;
+        //console.log(countDown);
+    }
+    renderCounterDiningRoomCheck();
+}
+
+
+function updateEmptyTrashCansTimer() {
+    if (emptyTrashCansCountDown > 0) {
+        emptyTrashCansCountDown--;
+        //console.log(countDown);
+    }
+    renderCounterEmptyTrashCans();
+}
+
+
+function renderCounterIceForDriveThru() {
+    const counterIceForDriveThru = document.getElementById("iceForDriveThruCounter");
+    counterIceForDriveThru.innerHTML = iceForDriveThruCountDown;
+}
+
+function renderCounterDiningRoomCheck() {
+    const counterDiningRoomCheck = document.getElementById("diningRoomCheckCounter");
+    counterDiningRoomCheck.innerHTML = diningRoomCheckCountDown;
+}
+
+function renderCounterEmptyTrashCans() {
+    const counterEmptyTrashCans = document.getElementById("emptyTrashCansCounter");
+    counterEmptyTrashCans.innerHTML = emptyTrashCansCountDown;
 }
