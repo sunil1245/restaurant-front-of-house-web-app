9fe591d8e7cdc10a46770abc7716b2e6dbe89be7
sunil1245 <119752484+sunil1245@users.noreply.github.com>
2025-05-14 22:25:21 -0500

    Console log drive-thru info

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const timers = [
-    { id: "ice_to_drivethrough", duration: 30, label: "Take ice to drive through" },
+    { id: "ice_to_drivethrough", duration: 20, label: "Take ice to drive through" },
     { id: "empty_trash_cah", duration: 30, label: "Empty the Trash Can" },
 ]
 
@@ -11,9 +11,13 @@ const timers = [
     Appending elements to the document
 */ 
 
+console.log("Ice for Drive-Thru ID", timers[0].id);
+console.log("Ice for Drive-Thru duration", timers[0].duration);
+console.log("Ice for Drive-Thru label", timers[0].label);
+
 
 //const setTaskInterval = setInterval(getIceForDriveThru, 5000);
-let iceForDriveThruCountDown = 20;
+let iceForDriveThruCountDown = timers[0].duration;
 let diningRoomCheckCountDown = 30;
 let emptyTrashCansCountDown = 45;
 
@@ -28,11 +32,11 @@ setInterval(updateDiningRoomCheckTimer, 1000);
 setInterval(updateEmptyTrashCansTimer, 1000);
 
 function getIceForDriveThru() {
-    alert("Take ice to Drive-Thru");
+    alert(timers[0].label);
 }
 
 function resetIceDriveThruCounter() {
-    iceForDriveThruCountDown = 20;
+    iceForDriveThruCountDown = timers[0].duration;
     renderCounterIceForDriveThru();
 }
 
