e38671c200a057909125c0ddad9b706996f147a9
sunil1245 <119752484+sunil1245@users.noreply.github.com>
2025-08-24 15:22:03 -0500

    Update app.js
    Add resetCounters

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const timers = [
     { id: "dining_room_check", duration: 45, label: "Dining Room Check" },
     { id: "empty_trash_cans", duration: 30, label: "Empty the Trash Can" },
     // { id: "make_coffee", duration: 30, label: "Make Coffee" },
-{ id: "make_lemonade", duration: 50, label: "Make Lemonade" },
+    { id: "make_lemonade", duration: 50, label: "Make Lemonade" }   ,
 ]
 
 /*
@@ -25,6 +25,8 @@ const timers = [
 */ 
 
 renderCounters(timers);
+renderResetButtons(timers);
+
 
 setInterval(function() {
     updateTimers(timers)
@@ -32,10 +34,10 @@ setInterval(function() {
 
 
 function renderCounters(timers) {
-const timerContainer = document.getElementById('container');
-timerContainer.innerHTML = "";
+    const timerContainer = document.getElementById('container');
+            
+    timerContainer.innerHTML = "";
     for (let i = 0; i < timers.length; i++) {
-        
         timerContainer.innerHTML += 
         "<div class='timer-container'><div id='" + 
             timers[i].id + "'>" + 
@@ -43,24 +45,46 @@ timerContainer.innerHTML = "";
             "</div><div id = '" + 
             timers[i].id + 
             "_counter'>" + timers[i].duration + 
-        "</div></div>";
-    }
+        "</div>" +
+        "<button data-index=" + i + " class='reset-btn'>Reset</button></div>";
+    }   
 }
+
+function renderResetButtons(timers) {
+
+    const container = document.getElementById('container');
+
+
+    container.addEventListener('click', function(event) {
+
+        const resetButton = event.target.closest('.reset-btn');        
+        const index = parseInt(resetButton.getAttribute('data-index'));
+
+            if (resetButton && container.contains(resetButton)) {
+
+                originalDuration = timers[index].duration;
+                timers[index].duration = 0;
+                timers[index].duration = originalDuration;
+            }
+        });
+
+}
+
+
 
 function updateTimers(timers) {
     for (let i = 0; i < timers.length; i++) {
         
-        if (timers[i].duration > 0) {
-            timers[i].duration--;
-            // let timerCounter = document.getElementById(timers[i].id + '_counter');
-            // timerCounter.innerText = timers[i].duration;
-
-
-        }
+            if (timers[i].duration > 0) {
+                timers[i].duration--;
+                // let timerCounter = document.getElementById(timers[i].id + '_counter');
+                // timerCounter.innerText = timers[i].duration;
+            }
         
-    }
-renderCounters(timers);
-
+        }
+    renderCounters(timers);
 }
+
+
 
 
